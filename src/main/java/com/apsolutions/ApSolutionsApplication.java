7284eaaf139201e04036cac5b4cd2cbd1e48a095
Eloy Huallama Perez <ehuallamaperez@gmail.com>
2023-09-16 19:27:13 -0500

    inicio de proyecto

---
+++ src/main/java/com/apsolutions/ApSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.apsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApSolutionsApplication.class, args);
+	}
+
+}
