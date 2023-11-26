41c543b37b3c8771630ebbb0926cfa927df1a6f8
Eloy Huallama Perez <ehuallamaperez@gmail.com>
2023-11-26 16:36:26 -0500

    Formateo de código in ProductoFilterRepositoryImpl

--- src/main/java/com/apsolutions/repository/impl/ProductoFilterRepositoryImpl.java
+++ src/main/java/com/apsolutions/repository/impl/ProductoFilterRepositoryImpl.java
@@ -8,7 +8,6 @@ import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Objects;
 
 @Repository
 public class ProductoFilterRepositoryImpl implements ProductoFilterRepository {
@@ -16,37 +15,37 @@ public class ProductoFilterRepositoryImpl implements ProductoFilterRepository {
     private EntityManager entityManager;
 
     @Override
-    public List<ProductoListDto> filter(int tipo, String nombre, int idCategory, int idBrand){
+    public List<ProductoListDto> filter(int tipo, String nombre, int idCategory, int idBrand) {
         StringBuilder jpql = new StringBuilder("SELECT new com.apsolutions.dto.ProductoListDto(p.id, p.codigo, p.nombre, p.descripcion, c.nombre, m.nombre, p.precio, p.stock) " +
                 "FROM Producto p INNER JOIN p.categoria c INNER JOIN p.marca m WHERE p.estado = true ");
 
-        if (!Objects.equals(nombre, "")){
-            if (tipo == 1){
+        if (!nombre.isEmpty()) {
+            if (tipo == 1) {
                 jpql.append(" AND p.codigo = :nombre");
-            }else{
+            } else {
                 jpql.append(" AND p.nombre = :nombre");
             }
         }
 
-        if (idCategory > 0){
+        if (idCategory > 0) {
             jpql.append(" AND p.categoria.id = :idCategory");
         }
 
-        if (idBrand > 0){
+        if (idBrand > 0) {
             jpql.append(" AND p.marca.id = :idBrand");
         }
 
         TypedQuery<ProductoListDto> query = entityManager.createQuery(jpql.toString(), ProductoListDto.class);
 
-        if (!Objects.equals(nombre, "")){
+        if (!nombre.isEmpty()) {
             query.setParameter("nombre", nombre);
         }
 
-        if (idCategory > 0){
+        if (idCategory > 0) {
             query.setParameter("idCategory", idCategory);
         }
 
-        if (idBrand > 0){
+        if (idBrand > 0) {
             query.setParameter("idBrand", idBrand);
         }
 
